837519eea0b4182ee3dde163bd3cde4e84dec0f3
AntonelaDiPinto <dipintoantonela@gmail.com>
2024-11-15 21:38:09 -0300

    Se crea la clase Casino y la Interfaz Apuesta

---
+++ Casino.ts
@@ -0,0 +1,48 @@
+export class Casino {
+
+    private nombre: string;
+    private usuarios: Usuario[];
+    private juegos: Juego[];
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+        this.usuarios = [];
+        this.juegos = [];
+    }
+
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    getUsuarios(): Usuario[] {
+        return this.usuarios;
+    }
+
+    getJuegos(): Juego[] {
+        return this.juegos;
+    }
+
+    public setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    public mostrarCasino(): string {
+        return `Casino: ${this.getNombre()} \n Usuarios del Casino: ${this.getUsuarios()} \n Juegos del Casino: ${this.getJuegos()}`;
+    }
+
+    public listarNombres(arreglo: Usuario | Juego): String {
+        let cadena: string = ' ';
+        arreglo.forEach(a => {
+            cadena += `\n ${a.getNombre()}`
+        });
+        return cadena;
+    }
+
+    public darBienvenida(): void {
+        console.log(`Bienvenido al Casino ${this.getNombre()}`);
+    }
+
+    public despedir(): string {
+        return `${} gracias por elegir ${this.getNombre()}, volve pronto!!!`;// Agregar el nombre del Usuario
+    }
+}
