8d2a0874cf2769b6d657219ac6f4b7207b9a3fb1
Agustin Sau <sau.agustin.i@gmail.com>
2024-11-23 21:59:50 -0300

    comodin agregado a posibles valores de la barra

--- TragamonedaPremiun.ts
+++ TragamonedaPremiun.ts
@@ -1,24 +1,20 @@
 import { Tragamoneda } from "./Tragamoneda";
 
 export class TragamonedaPremium extends Tragamoneda {
-    private comodin: string;
-    readonly FACTOR_SUERTE: number = 5;
+    private comodin: string = "🃏"
+    readonly FACTOR_SUERTE: number = 3;
 
     constructor(intentosMaximos: number, numBarras: number, numPosiciones: number) {
         super(intentosMaximos, numBarras, numPosiciones);
-        this.comodin = "🃏";  
-        this.aniadirComodin();
-        //this.generarBarra(3). El comodiin se agrega a los posibles valores, pero la barra ya se genera al crear el objeto sin este...
+        //this.comodin = "🃏";  
+        this.agregarComodin();
+        this.generarBarra(numPosiciones); //El comodiin se agrega a los posibles valores
     }
 
-    public aniadirComodin(): void {
-        this.agregarValor(this.getComodin(), this.FACTOR_SUERTE); //Llama a Agregar valor de la clase padre
-    }
-
-    private agregarValor(valor: string, suerte: number): void { //agrega comodines segun cuanto valga el factor suerte
-        for (let index = 0; index < suerte; index++) {    
-            this.valores.push(valor);
-        }
+    public agregarComodin(): void {
+        for (let index = 0; index < this.FACTOR_SUERTE; index++) {    
+            this.valores.push(this.comodin);
+        } 
     }
 
     //METODO TIRAR CON LA POSIBILIDAD DE QUE SALGA EL COMODIN (SE PUEDE MEJORAR UN MONTOOOOON)
@@ -36,7 +32,7 @@ export class TragamonedaPremium extends Tragamoneda {
     }
 
     public mostrarResultado(): void {
-        const resultado = this.tirar();
+        const resultado = this.tirarPremium(); //Llamo a tirar premium en vez de tirar del padre
         if (resultado.length > 0) {
             console.log("Resultado de la tirada:", resultado.join(" | "));
             console.log("Intentos Restantes:", this.intentosMaximos);
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { TragamonedaPremium } from "./TragamonedaPremiun";
 import { Tragamoneda } from "./Tragamoneda";
 
-let tragamonedaPremium: TragamonedaPremium = new TragamonedaPremium(5, 3, 4);
+let tragamonedaPremium: TragamonedaPremium = new TragamonedaPremium(5, 3, 3);
+let maximoTiradas: number = tragamonedaPremium.getIntentosMaximos();
 
-for (let index = 0; index <= tragamonedaPremium.getIntentosMaximos(); index++) {
+for (let index = 0; index <= maximoTiradas; index++) {
     tragamonedaPremium.mostrarResultado();
-
     
 }
 
