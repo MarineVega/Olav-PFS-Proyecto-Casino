62bf0c609220445704348a87585ec25524879c56
nataliagelmi <nataliagelmi@gmail.com>
2024-11-28 20:50:40 -0300

    Menu terminado para agregar al principal

--- index.ts
+++ index.ts
@@ -4,22 +4,65 @@ import { HorasEspejo } from "./HorasEspejo";
 import { HorasEspejoSolitario } from "./HorasEspejoSolitario";
 
 // Crear al usuario
-const jugador = new Usuario("Naty","Natalia", 200); // Jugador con saldo inicial fijo
+const jugador = new Usuario("Naty","Natalia", 10000); // Jugador con saldo inicial fijo
+console.log(" ");
+console.log(" *****************12:21***************** HORAS 00:00 ESPEJO ****************15:51*************************");
 
 console.log("Selecciona un juego:");
-console.log("1. Horas Espejo (Jugador vs Máquina)");
-console.log("2. Horas Espejo Solitario");
+console.log("1. Horas Espejo ⚔️  (Jugador vs Máquina)");
+console.log("2. Horas Espejo Solitario 🙃");
 const opcion = readline.questionInt("Opcion: ");
+
+
+let apuesta: number=0;
+let apuestaValida: boolean;
+
+
+console.log(" ")
+console.warn("Dinero disponible del usuario: " + jugador.obtenerSaldo());
+console.log(" ")
+console.log(" ")
+
+
 
 // Instanciar el juego correspondiente
 if (opcion === 1) {
-    const horasEspejo = new HorasEspejo("Horas Espejo", "Reglas.", 100, 1000, jugador);
-    horasEspejo.iniciarPartida();
+    const horasEspejo = new HorasEspejo("Horas Espejo", "Apuesta minima de $1000, ganas con 30 puntos o con 00:00 multiplicas tu apuesta por 10!!", 1000, 5000, jugador);
+    console.log(`Reglamento: ${horasEspejo.getReglamento()}`);
+    do {
+        apuesta = readline.questionInt("Ingrese el dinero de la apuesta: ");
+        apuestaValida = horasEspejo.apostar(apuesta);
+    } while (!apuestaValida)
+        console.warn("Dinero disponible del usuario: " + jugador.obtenerSaldo());
+
+        if (apuestaValida) {
+            console.log("  ")
+            console.error("🎮Presione cualquier tecla para comenzar: ");
+            readline.question();         
+            horasEspejo.iniciarPartida();
+        }
+
 } else if (opcion === 2) {
-    const horasEspejoSolitario = new HorasEspejoSolitario("Horas Espejo Solitario", "Reglas.", 50, 500, jugador);
+   
+   
+    const horasEspejoSolitario = new HorasEspejoSolitario("Horas Espejo Solitario", "Apuesta Minima $1000, ¿Que te dira el azar?, saca horas espejo y sumas puntos con 30 puntos ganas o con 00:00 ganas tu apuesta por 10!!.", 1000, 5000, jugador);
+    console.log(`Reglamento: ${horasEspejoSolitario.getReglamento()}`);
     horasEspejoSolitario.iniciarPartida();
+    do {
+        apuesta = readline.questionInt("Ingrese el dinero de la apuesta: ");
+        apuestaValida = horasEspejoSolitario.apostar(apuesta);
+    } while (!apuestaValida)
+        
+        console.warn("Dinero disponible del usuario: " + jugador.obtenerSaldo());
+
+        if (apuestaValida) {
+            console.log("  ")
+            console.error("🎮Presione cualquier tecla para comenzar: ");
+            readline.question();         
+            horasEspejoSolitario.iniciarPartida();
+        }
 } else {
     console.log("Opción no válida.");
 }
 
-console.log(`Saldo final del jugador: $${jugador.obtenerSaldo()}`);
+
