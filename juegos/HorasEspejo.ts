2cb89fc92a9bf80e59a1bd04ec019574c17cd32e
natalia gelmi <nataliagelmi@gmail.com>
2024-12-03 14:05:26 -0300

    Se agrega la clase abstracta HorasRelojBase y se modifican los juegos para que reutilicen el codigo

--- juegos/HorasEspejo.ts
+++ juegos/HorasEspejo.ts
@@ -1,94 +1,39 @@
-import { Juego } from "../abstractas/Juego";
+import { HorasEspejoBase } from "../abstractas/HorasEspejoBase";
 import { Usuario } from "../clases/Usuario";
 import * as readline from "readline-sync";
 
-export class HorasEspejo extends Juego {
 
-    private intentosJugador: number;
+export class HorasEspejo extends HorasEspejoBase {
     private intentosMaquina: number;
     private puntosAcumuladosMaquina: number;
-    private puntosAcumulados: number;
-    private puntosMayor: number;
-    private horasEspejo: string[];
-    private puntos10: string[];
-    private puntos25: string[];
-    private puntosSimples: number;
-    private puntosDobles: number;
-    protected jugador: Usuario;
 
     constructor(nombre: string, reglamento: string, apuMin: number, apuMax: number, jugador: Usuario) {
         super(nombre, reglamento, apuMin, apuMax, jugador);
-
-        this.puntosAcumuladosMaquina = 0;
-        this.puntosAcumulados = 0;
-        this.intentosJugador = 3;
         this.intentosMaquina = 3;
-        this.puntosMayor = 50;
-        this.jugador = jugador;
-        this.puntosSimples = 10;
-        this.puntosDobles = 25;
-
-        this.horasEspejo = [
-            "01:10", "02:20", "03:30", "04:40", "05:50",
-            "10:01", "11:11", "12:21", "13:31", "14:41",
-            "15:51", "20:02", "21:12", "22:22", "23:32", "00:00"];
-
-        this.puntos10 = [
-            "02:20", "03:30", "04:40", "05:50", "10:01",
-            "12:21", "13:31", "14:41", "15:51", "20:02",
-            "21:12", "23:32"];
-
-        this.puntos25 = ["11:11", "22:22"];
+        this.puntosAcumuladosMaquina = 0;
     }
 
-    // GENERAR HORAS
-    private generarHoraEspejo(): string {
-        let hora: number;
-        let minutos: number;
-        const generarEspejo = Math.random() < 0.3; // 30% de probabilidad de generar una hora espejo
-        if (generarEspejo) {
-            const indiceAleatorio = Math.floor(Math.random() * this.horasEspejo.length);
-            return this.horasEspejo[indiceAleatorio];
-        }
-        hora = Math.floor(Math.random() * 24); // Hora entre 0 y 23
-        minutos = Math.floor(Math.random() * 60); // Minutos entre 0 y 59
-        const horaFormateada = hora < 10 ? `0${hora}` : `${hora}`;
-        const minutosFormateados = minutos < 10 ? `0${minutos}` : `${minutos}`;
-        
-        return `${horaFormateada}:${minutosFormateados}`;
-    }
-    //JUEGA EL TURNO
     private jugarTurno(quienJuega: string): void {
-        const horaFormateada = this.generarHoraEspejo();
-        let puntos = 0;
-        let dinero = 0;
+        const horaFormateada = this.generarHoraAleatoria();
 
-        // Condición de victoria especial
         if (horaFormateada === "00:00") {
-            dinero = this.getApuesta() * 10;
-            console.log(`🎉 ¡${quienJuega === 'jugador' ? 'El jugador' : 'La máquina'} GANA con 🕛"00:00"🎉!`);
+            console.log(`🎉 ¡${quienJuega === 'jugador' ? 'El jugador' : 'La máquina'} GANA con 🕛"00:00"! 🎉`);
+            const premio = this.getApuesta() * 10;
 
             if (quienJuega === 'jugador') {
                 this.puntosAcumulados += this.puntosMayor;
-                this.pagarApuesta(dinero);
-                this.intentosJugador = 0;
-                this.intentosMaquina = 0;
+                this.pagarApuesta(premio);
             } else {
                 this.puntosAcumuladosMaquina += this.puntosMayor;
-                this.intentosJugador = 0;
-                this.intentosMaquina = 0;
             }
-            return; // Termina la ejecución
+
+            this.intentosJugador = 0;
+            this.intentosMaquina = 0;
+            return; // Termina el juego
         }
 
-        // Asignación de puntos por coincidencias
-        if (this.puntos10.indexOf(horaFormateada) !== -1) {
-            puntos = this.puntosSimples;
-        } else if (this.puntos25.indexOf(horaFormateada) !== -1) {
-            puntos = this.puntosDobles;
-        }
-
-        // Actualización de puntajes e intentos
+        // Continúa con el cálculo de puntos regulares
+        const puntos = this.determinarPuntaje(horaFormateada);
         if (quienJuega === 'jugador') {
             this.puntosAcumulados += puntos;
             this.intentosJugador--;
@@ -97,72 +42,25 @@ export class HorasEspejo extends Juego {
             this.intentosMaquina--;
         }
 
-        // Mensaje de estado
-        console.log(`${quienJuega === 'jugador' ? 'Jugador' : 'Máquina'}: ⏰ ${horaFormateada}, Puntos 👉: ${puntos}`);
+        console.log(`${quienJuega === 'jugador' ? 'Jugador' : 'Máquina'}: ⏰ ${horaFormateada}, Puntos: ${puntos}`);
     }
 
-    
-    //INICIA LA PARTIDA CONTROLANDO LAS RONDAS
+
     public jugar(): void {
         this.mostrarInfoComienzoJuego();
-
         let continua: boolean = true;
-
-        if(this.apostar()){
-            this.mostrarInfoCobroEntrada();
-
-            console.log("\n--- 🍀 INICIA LA PARTIDA 🍀---");
-    
+        if (this.apostar()) {
             let ronda = 1;
-
-            while (this.intentosJugador !== 0) {
-                console.log(`\n--- ↪️ Ronda ${ronda} ↩️---`);
-    
-                // Turno del jugador
-                if (this.intentosJugador > 0) {
-                    this.jugarTurno('jugador');
-                } else {
-                    console.log("El jugador ya no tiene intentos restantes.");
-                }
-    
-                // Turno de la máquina
-                if (this.intentosMaquina > 0) {
-                    this.jugarTurno('maquina');
-                } else {
-                    //console.log("La máquina ya no tiene intentos restantes.");
-                }
-    
-                if (this.intentosJugador > 0){
-                    console.log("Presione una tecla para continuar..., 🍀Suerte!!");
-                    readline.question();
-                }
-
+            while (this.intentosJugador > 0 || this.intentosMaquina > 0) {
+                console.log(`\n--- Ronda ${ronda} ---`);
+                if (this.intentosJugador > 0) this.jugarTurno('jugador');
+                if (this.intentosMaquina > 0) this.jugarTurno('maquina');
+                readline.question("Presione Enter para continuar...");
                 ronda++;
             }
-    
             this.mostrarPuntajeTotal();
-
-            // Preguntar si desea jugar nuevamente
-            continua = this.preguntarSiContinua();
-                    
-            if(continua){
-                console.log(`\nJugaras otra ronda.\n`);
-                this.restablecerJuego();
-                this.jugar();
-                
-            } else {
-                console.log(`Has elegido salir del juego\n`);
-                console.log(`Saldo final en tu Billetera:💲${this.jugador.obtenerSaldo()}`);
-            }
-
-        } else {
-            
-            if(this.verifcarBilletera()){
-                this.jugar();
-            }
         }
     }
-
 
     // Mostrar puntajes finales
     private mostrarPuntajeTotal(): void {
@@ -178,21 +76,24 @@ export class HorasEspejo extends Juego {
         } else if (this.puntosAcumulados < this.puntosAcumuladosMaquina) {
             console.log("🤖 ¡La máquina gana!");
             console.log(`Saldo final en tu Billetera:💲${this.jugador.obtenerSaldo()}`);
-            
-                       
+
 
         } else {
             console.log("🤗¡Es un empate!, recuperaste tu apuesta");
             this.pagarApuesta(this.getApuesta())
 
         }
-        
-    }
 
+        // Preguntar si desea continuar jugando
+        if (this.preguntarSiContinua()) {
+            this.jugar(); // Jugar otra ronda
+        }
+    }
     public restablecerJuego(): void {
         this.intentosJugador = 3;
         this.intentosMaquina = 3;
         this.puntosAcumulados = 0;
         this.puntosAcumuladosMaquina = 0;
     }
+
 }
--- juegos/HorasEspejoSolitario.ts
+++ juegos/HorasEspejoSolitario.ts
@@ -1,101 +1,52 @@
-import { Juego } from "../abstractas/Juego";
+import { HorasEspejoBase } from "../abstractas/HorasEspejoBase";
 import { Usuario } from "../clases/Usuario";
 import * as readline from "readline-sync";
 
-export class HorasEspejoSolitario extends Juego {
-    private intentosJugador: number;
-    private puntosAcumulados: number;
-    private puntosNumIguales: number;
-    private puntosMayor:number;
-    private puntosEspejo: number;
-    private puntosGanador:number;
-    protected jugador: Usuario;
-
+export class HorasEspejoSolitario extends HorasEspejoBase {
+    private puntosGanador: number;
     constructor(nombre: string, reglamento: string, apuMin: number, apuMax: number, jugador: Usuario) {
         super(nombre, reglamento, apuMin, apuMax, jugador);
-        this.intentosJugador = 3;
-        this.puntosAcumulados = 0;
-        this.jugador = jugador;
-        this.puntosNumIguales= 20;
-        this.puntosMayor=50;
-        this.puntosEspejo=10;
-        this.puntosGanador=30;
+        this.puntosGanador = 30;
     }
 
-    private generarHoraAleatoria(): string {
-        //SINO HARdCODEO UN POCO HAY QUE HACER MILLONES DE PARTIDAS 
-        const horasEspejo = [
-            "01:10", "02:20", "03:30", "04:40", "05:50",
-            "10:01", "11:11", "12:21", "13:31", "14:41",
-            "15:51", "20:02", "21:12", "22:22", "23:32", "00:00"
-        ];
-    
-        // Probabilidad de generar una hora espejo (30%)
-        const probabilidadEspejo = 0.3; 
-    
-        if (Math.random() < probabilidadEspejo) {
-            // Selecciona una hora espejo aleatoria
-            const indice = Math.floor(Math.random() * horasEspejo.length);
-            return horasEspejo[indice];
-        }
-    
-        // Genera una hora completamente aleatoria
-        const hora = Math.floor(Math.random() * 24); // Hora entre 0 y 23
-        const minutos = Math.floor(Math.random() * 60); // Minutos entre 0 y 59
-        const horaFormateada = hora < 10 ? `0${hora}` : `${hora}`;
-        const minutosFormateados = minutos < 10 ? `0${minutos}` : `${minutos}`;
-        
-        return `${horaFormateada}:${minutosFormateados}`;
-    }
-    
-    //DETERMINO QUE SEA UNA HORA ESPEJO COMPARANDO LAS POSICIONES DEL STRING
-    private determinarPuntaje(hora: string): number {
-        // VERIFICO QUE SEA UNA HORA ESPEJO
-        if (hora[0] === hora[4] && hora[1] === hora[3]) {
-            switch (hora) {
-                case "00:00":
-                    return this.puntosMayor; // Puntaje máximo para "00:00"
-                case "11:11":
-                case "22:22":
-                    return this.puntosNumIguales; // Puntaje especial para "11:11" y "22:22"
-                default:
-                    return this.puntosEspejo; // PUNTAJE RESTO HORAS ESPEJO
-            }
-        }
-
-        return 0; //NO ES UNA HORA ESPEJO
-    }
-
-
-    // EJECUTA UN TURNO, HASTA 3 POSIBILIDADES 
     private jugarTurno(): boolean {
         const horaFormateada = this.generarHoraAleatoria();
-        const puntos = this.determinarPuntaje(horaFormateada);
-        //MAXIMO PUNTAJE     
-        if (puntos === this.puntosMayor) {
+
+        // Si la hora es "00:00" gana la partida y el premio  mayor
+         if (horaFormateada === "00:00") {
             console.log(`🎉 ¡GANASTE con 🕛"00:00"! 🎉`);
-            this.puntosAcumulados += this.puntosMayor; // Puntos máximos al ganar con "00:00"
-            this.intentosJugador = 0;
-            return true; // SI GANO CON 00:00 DEBE TERMINAR
+            this.puntosAcumulados += this.puntosMayor;
+            const premio = this.getApuesta() * 10;
+            this.pagarApuesta(premio);
+            this.intentosJugador = 0; // Detiene el juego
+            return true; // Termina el juego si es "00:00"
         }
 
+        // Calcular puntos regulares
+        const puntos = this.determinarPuntaje(horaFormateada);
         this.puntosAcumulados += puntos;
         this.intentosJugador--;
-
         console.log(`⏰ Hora: ${horaFormateada}, Puntos obtenidos: ${puntos}`);
         console.log(`Puntos acumulados: ${this.puntosAcumulados}`);
-        return this.puntosAcumulados >= this.puntosGanador; // ACA GANA POR PUNTOS ACUMULADOS
+
+        // Condición de victoria por puntos (30 o más, pero menos de 50)
+        if (this.puntosAcumulados >= 30 && this.puntosAcumulados < this.puntosMayor) {
+            console.log(`🎉 ¡Ganaste al alcanzar ${this.puntosAcumulados} puntos! 🎉`);
+            const premio = this.getApuesta() * 2;
+            this.pagarApuesta(premio);
+            this.intentosJugador = 0; // Detiene el juego
+            return false; // No detener el juego aquí, porque puede seguir jugando si hay intentos
+        }
+
+        return false; // Continuar jugando si no ha ganado
     }
 
-    // Iniciar la partida
     public jugar(): void {
+       
         this.mostrarInfoComienzoJuego();
 
-        let sigueJugando: string;
-
-        if(this.apostar()){
+        if (this.apostar()) {
             this.mostrarInfoCobroEntrada();
-            
             console.log("\n--- 🍀 INICIA LA PARTIDA 🍀 ---");
 
             while (this.intentosJugador > 0) {
@@ -105,15 +56,7 @@ export class HorasEspejoSolitario extends Juego {
 
                 const victoria = this.jugarTurno();
                 if (victoria) {
-                    console.log("🎉 ¡Ganaste al alcanzar 30 puntos o más! 🎉");
-                    if ((this.puntosAcumulados >= this.puntosGanador) && (this.puntosAcumulados < 50)) {
-                        this.pagarApuesta(this.getApuesta() * 2); // Premiar con el doble de la apuesta mínima
-                        
-                    }
-                    else if (this.puntosAcumulados >= this.puntosMayor) {
-                        this.pagarApuesta(this.getApuesta() * 10); //el premio mayor
-                        break;
-                    }
+                    break; // Termina el juego solo si el jugador gana con "00:00"
                 }
             }
 
@@ -123,31 +66,19 @@ export class HorasEspejoSolitario extends Juego {
                 console.log('____________________________________________________________')
             }
 
-        
             console.log(`Puntaje final: ${this.puntosAcumulados}`);
             console.log(`Saldo final del jugador💸: ${this.jugador.obtenerSaldo()}`);
-            
-            // Preguntar si desea jugar nuevamente
-            sigueJugando = readline.question("Desea jugar otra vez? S/N: ");
-
-            if (sigueJugando.toLocaleLowerCase() === 'n'){
-                console.log(`\nHas elegido salir del juego`); 
-
-            } else {
-                console.log(`\nJugaras otra ronda.`);
-                this.restablecerJuego();
-                this.jugar();
-            }
-        } else {
-            
-            if(this.verifcarBilletera()){
-                this.jugar();
-            }
+        
+         // Preguntar si desea continuar jugando
+         if (this.preguntarSiContinua()) {
+            this.jugar(); // Jugar otra ronda
+        }
+      
         }
     }
-
     public restablecerJuego(): void {
         this.intentosJugador = 3;
         this.puntosAcumulados = 0;
     }
 }
+
